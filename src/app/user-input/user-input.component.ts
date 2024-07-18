5c98f4cb2cfc648cb9736723c9d2caeb786b48bc
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-07-18 10:14:11 +0200

    feat: Cross-Component Communication w/ Output()

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,16 +9,24 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number,
+    duration: number,
+    expectedReturn: number,
+    annualInvestment: number,
+  }>();
+
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
-    console.log('SUBMITTED!');
-    console.log('Initial investment: ', this.enteredInitialInvestment);
-    console.log('Annual investment: ', this.enteredAnnualInvestment);
-    console.log('Expected return: ', this.enteredExpectedReturn);
-    console.log('Duration: ', this.enteredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
   }
 }
